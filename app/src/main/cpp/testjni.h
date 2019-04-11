e6f46063c735579cc11af3f387df3f10f113d54e
Siddarth <siddarthgandhi@gmail.com>
2019-04-11 09:26:29 -0400

    Initial commit

---
+++ app/src/main/cpp/testjni.h
@@ -0,0 +1,12 @@
+//
+// Created by Siddarth Gandhi on 4/11/19.
+//
+
+#include <jni.h>
+
+#ifndef MBEDTLS_ANDROID_TESTJNI_H
+#define MBEDTLS_ANDROID_TESTJNI_H
+
+#endif //MBEDTLS_ANDROID_TESTJNI_H
+
+jstring Java_com_simplisafe_mbedtls_MainActivity_printTest(JNIEnv *, jobject);
